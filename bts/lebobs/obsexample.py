587d652d7a3e5304245a5249efa51ba5b84f69f0
EightyNine <EightyNine@EightyNine.me>
2022-05-11 10:10:45 +0200

    cleanup

--- bts/lebobs/obsexample.py
+++
@@ -1,9 +0,0 @@
-from . import lebobs
-
-if __name__ == "__main__":
-    lebobs.switchToscene("Scenename")
-    lebobs.setTransition("Transitionname")
-    lebobs.startRec()
-    lebobs.stopRec()
-    lebobs.startStream()
-    lebobs.stopStream()
